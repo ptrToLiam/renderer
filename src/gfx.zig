aeac13c42e76f8de9e2f9d3d2e78d0e13d7f8f86
Liam Malone <maloneliam@proton.me>
2025-10-02 13:34:26 +0100

    Working toward more vectorizing

--- src/gfx.zig
+++ src/gfx.zig
@@ -3,23 +3,23 @@ const math = @import("math");
 pub const Image = []Pixel;
 
 pub const Pixel = u32;
-pub const Color = packed struct (u32) {
+pub const Color = packed struct(u32) {
     b: u8 = 0,
     g: u8 = 0,
     r: u8 = 0,
     a: u8 = 0,
 
     pub const red: Color = .{
-         .r = 255,
-         .a = 255,
+        .r = 255,
+        .a = 255,
     };
     pub const blue: Color = .{
-         .b = 255,
-         .a = 255,
+        .b = 255,
+        .a = 255,
     };
     pub const green: Color = .{
-         .g = 255,
-         .a = 255,
+        .g = 255,
+        .a = 255,
     };
     pub const yellow: Color = .{
         .r = 255,
@@ -107,5 +107,95 @@ pub fn trace_ray(origin: Vec3f32, direction: Vec3f32, spheres: []const Sphere, m
     const sphere = closest_sphere orelse return null;
     return sphere.color;
 }
+
+pub fn trace_ray_vectorized(origin: Vec3f32, direction: Vec3f32, spheres: []const Sphere, min_t: f32, max_t: f32) ?Color {
+    const min_ts: @Vector(4, f32) = @splat(min_t);
+    const max_ts: @Vector(4, f32) = @splat(max_t);
+
+    const colors: @Vector(4, u32) = .{
+        @bitCast(spheres[0].color),
+        @bitCast(spheres[1].color),
+        @bitCast(spheres[2].color),
+        @bitCast(spheres[3].color),
+    };
+
+    const ox_vec: @Vector(4, f32) = @splat(origin[0]);
+    const oy_vec: @Vector(4, f32) = @splat(origin[1]);
+    const oz_vec: @Vector(4, f32) = @splat(origin[2]);
+    const dx_vec: @Vector(4, f32) = @splat(direction[0]);
+    const dy_vec: @Vector(4, f32) = @splat(direction[1]);
+    const dz_vec: @Vector(4, f32) = @splat(direction[2]);
+
+    const sx_vec: @Vector(4, f32) = .{
+        spheres[0].center[0],
+        spheres[1].center[0],
+        spheres[2].center[0],
+        spheres[3].center[0],
+    };
+    const sy_vec: @Vector(4, f32) = .{
+        spheres[0].center[1],
+        spheres[1].center[1],
+        spheres[2].center[1],
+        spheres[3].center[1],
+    };
+    const sz_vec: @Vector(4, f32) = .{
+        spheres[0].center[2],
+        spheres[1].center[2],
+        spheres[2].center[2],
+        spheres[3].center[2],
+    };
+    const radii_vec: @Vector(4, f32) = .{
+        spheres[0].radius,
+        spheres[1].radius,
+        spheres[2].radius,
+        spheres[3].radius,
+    };
+
+    const ocx = ox_vec - sx_vec;
+    const ocy = oy_vec - sy_vec;
+    const ocz = oz_vec - sz_vec;
+
+    const k1s: @Vector(4, f32) = @splat(math.dot(direction, direction));
+    const k2s = (@as(@Vector(4, f32), @splat(2)) *
+        ((ocx * dx_vec) + (ocy * dy_vec) + (ocz * dz_vec)));
+    const k3s = (((ocx * ocx) + (ocy * ocy) + (ocz * ocz)) - (radii_vec * radii_vec));
+
+    const discriminants = ((k2s * k2s) -
+        (@as(@Vector(4, f32), @splat(4))) * k1s * k3s);
+    const disc_mask = discriminants >= @as(@Vector(4, f32), (@splat(0)));
+
+    const disc_sqrts = @sqrt(
+        @select(
+            f32,
+            disc_mask,
+            discriminants,
+            @as(@Vector(4, f32), @splat(0)),
+        ),
+    );
+
+    const denoms = @as(@Vector(4, f32), @splat(2)) * k1s;
+
+    var t1s: @Vector(4, f32) = (-k2s + disc_sqrts) / denoms;
+    var t2s: @Vector(4, f32) = (-k2s - disc_sqrts) / denoms;
+
+    const t1s_mask = disc_mask & (t1s >= min_ts) & (t1s <= max_ts);
+    const t2s_mask = disc_mask & (t2s >= min_ts) & (t2s <= max_ts);
+
+    t1s = @select(f32, t1s_mask, t1s, max_ts);
+    t2s = @select(f32, t2s_mask, t2s, max_ts);
+
+    const t_mins = @min(t1s, t2s);
+    const t_min = @reduce(.Min, t_mins);
+
+    if (t_min >= max_t) return null;
+
+    for (0..4) |idx| {
+        if (t_mins[idx] == t_min) {
+            return @bitCast(colors[idx]);
+        }
+    }
+    unreachable;
+}
+
 pub const Vec2f32 = math.Vec2f32;
 pub const Vec3f32 = math.Vec3f32;
--- src/main.zig
+++ src/main.zig
@@ -142,8 +142,10 @@ pub fn main() !void {
     try conn.flush();
 
     var frame_idx: usize = 0;
+    var red: u8 = 0;
 
     while (!wayland_state.should_close) : (frame_idx += 1) {
+        red = @intCast(frame_idx % 255);
         const frame_time_us = (std.time.us_per_ms * 32); // 32 ms per frame
         const frame_start_us = std.time.microTimestamp();
 
@@ -161,105 +163,129 @@ pub fn main() !void {
             const size = height * stride;
             _ = size;
 
-            // Sphere Drawing
+            const bg_color: gfx.Color = .{
+                .r = red,
+                .g = 255,
+                .b = 255,
+                .a = 255,
+            };
+
+            const vp_size = 1;
+            const proj_plane_z: f32 = 1;
+            const cam_pos: Vec3f32 = @splat(0);
+            const spheres: [4]Sphere = .{
+                .{ .center = .{ 0, -1, 4 }, .radius = 1, .color = .red },
+                .{ .center = .{ -2, 0, 4 }, .radius = 1, .color = .green },
+                .{ .center = .{ 2, 0, 4 }, .radius = 1, .color = .blue },
+                .{ .center = .{ 0, -5001, 0 }, .radius = 5000, .color = .yellow },
+            };
+
+            // clear bg
             {
-                const bg_color: gfx.Color = .{
-                    .r = 255,
-                    .g = 255,
-                    .b = 255,
-                    .a = 255,
-                };
+                if (std.simd.suggestVectorLength(u32)) |simd_width| {
+                    const simd_clear_start = std.time.microTimestamp();
+                    var idx: usize = 0;
+                    while (idx < img.len) {
+                        if (idx + simd_width < img.len) {
+                            defer idx += simd_width;
 
-                const vp_size = 1;
-                const proj_plane_z: f32 = 1;
-                const cam_pos: Vec3f32 = @splat(0);
-                const spheres: [4]Sphere = .{
-                    .{ .center = .{ 0, -1, 4 }, .radius = 1, .color = .red },
-                    .{ .center = .{ -2, 0, 4 }, .radius = 1, .color = .green },
-                    .{ .center = .{ 2, 0, 4 }, .radius = 1, .color = .blue },
-                    .{ .center = .{ 0, -5001, 0 }, .radius = 5000, .color = .yellow },
-                };
+                            var vec = std.mem.bytesToValue(
+                                @Vector(simd_width, u32),
+                                img[idx..][0..simd_width],
+                            );
 
-                // clear bg
-                {
-                    if (std.simd.suggestVectorLength(u32)) |simd_width| {
-                        const simd_clear_start = std.time.microTimestamp();
-                        var idx: usize = 0;
-                        while (idx < img.len) {
-                            if (idx + simd_width < img.len) {
-                                defer idx += simd_width;
-                                var vec: @Vector(simd_width, u32) = std.mem.bytesToValue(@Vector(simd_width, u32), img[idx..][0..simd_width]);
-                                vec = @splat(@bitCast(bg_color));
-                            } else {
-                                @branchHint(.cold);
-                                while (idx < img.len) : (idx += 1) {
-                                    img[idx] = @bitCast(bg_color);
-                                }
+                            vec = @splat(@bitCast(bg_color));
+                        } else {
+                            @branchHint(.cold);
+                            while (idx < img.len) : (idx += 1) {
+                                img[idx] = @bitCast(bg_color);
                             }
                         }
-                        const simd_clear_end = std.time.microTimestamp();
-                        const simd_clear_time = simd_clear_end - simd_clear_start;
-                        app_log.info("SIMD screen clear in {d}us", .{simd_clear_time});
-                    } else {
-                        // No SIMD -- Scalar fallback
-                        const memset_clear_start = std.time.microTimestamp();
-                        @memset(img, @bitCast(bg_color));
-                        const memset_clear_end = std.time.microTimestamp();
-                        const memset_clear_time = memset_clear_end - memset_clear_start;
-                        app_log.info("memset screen clear in {d}us", .{memset_clear_time});
                     }
+                    const simd_clear_end = std.time.microTimestamp();
+                    const simd_clear_time = simd_clear_end - simd_clear_start;
+                    app_log.info("SIMD screen clear in {d}us", .{simd_clear_time});
+                } else {
+                    // No SIMD -- Scalar fallback
+                    const memset_clear_start = std.time.microTimestamp();
+                    @memset(img, @bitCast(bg_color));
+                    const memset_clear_end = std.time.microTimestamp();
+                    const memset_clear_time = memset_clear_end - memset_clear_start;
+                    app_log.info("memset screen clear in {d}us", .{memset_clear_time});
                 }
-                
+            }
+
+            // Draw Scene
+            {
                 const half_width = @divFloor(width, 2);
                 const half_height = @divFloor(height, 2);
 
+                var ray_trace_time_sum: i128 = 0;
+                var ray_trace_count: i128 = 0;
                 var x: i32 = -half_width;
                 while (x < half_width) : (x += 1) {
-                    for (0..@intCast(height)) |y_iter| {
-                        const y: i32 = @as(i32, @intCast(y_iter)) - half_height;
-                        const dir = gfx.canvas_to_viewport(
-                            &.{
-                                .width = vp_size,
-                                .height = vp_size,
-                                .x = 0,
-                                .y = 0,
-                            },
-                            &.{
-                                .width = width,
-                                .height = height,
-                            },
-                            x,
-                            y,
-                            proj_plane_z,
-                        );
+                    var height_idx: usize = 0;
+                    while (height_idx < height) {
+                        if (std.simd.suggestVectorLength(i32)) |simd_width| {
+                            defer height_idx += simd_width;
+                            const vp_width: f32 = @floatFromInt(vp_size);
+                            const vp_height: f32 = @floatFromInt(vp_size);
+                            const x_float: f32 = @floatFromInt(x);
+                            const y_float: f32 = @as(f32, @floatFromInt(height_idx)) - (@as(f32, @floatFromInt(half_height)));
+                        } else {
+                            defer height_idx += 1;
+                            const y: i32 = @as(i32, @intCast(height_idx)) - half_height;
+                            const dir = gfx.canvas_to_viewport(
+                                &.{
+                                    .width = vp_size,
+                                    .height = vp_size,
+                                    .x = 0,
+                                    .y = 0,
+                                },
+                                &.{
+                                    .width = width,
+                                    .height = height,
+                                },
+                                x,
+                                y,
+                                proj_plane_z,
+                            );
 
-                        const col_opt = gfx.trace_ray(
-                            cam_pos,
-                            dir,
-                            &spheres,
-                            1,
-                            1000,
-                        );
-                        const color = if (col_opt) |col| col: {
-                            break :col col;
-                        } else bg_color;
-                        const idx: usize = idx: {
-                            const idx_x = half_width + x;
-                            const idx_y = half_height - y - 1;
-                            if (idx_x < 0 or
-                                idx_x >= width or
-                                idx_y < 0 or
-                                idx_y >= height)
-                            {
-                                continue;
-                            }
+                            const ray_trace_start = std.time.nanoTimestamp();
+                            const col_opt = gfx.trace_ray_vectorized(
+                                cam_pos,
+                                dir,
+                                &spheres,
+                                1,
+                                1000,
+                            );
+                            const ray_trace_end = std.time.nanoTimestamp();
+                            const ray_trace_time = ray_trace_end - ray_trace_start;
+                            ray_trace_time_sum += ray_trace_time;
+                            ray_trace_count += 1;
 
-                            break :idx @intCast(idx_y * width + idx_x);
-                        };
+                            const color = col_opt orelse bg_color;
+                            const idx: usize = idx: {
+                                const idx_x = half_width + x;
+                                const idx_y = half_height - y - 1;
+                                if (idx_x < 0 or
+                                    idx_x >= width or
+                                    idx_y < 0 or
+                                    idx_y >= height)
+                                {
+                                    continue;
+                                }
 
-                        img[idx] = @bitCast(color);
+                                break :idx @intCast(idx_y * width + idx_x);
+                            };
+
+                            img[idx] = @bitCast(color);
+                        }
                     }
                 }
+
+                const ray_trace_time_avg = @divFloor(ray_trace_time_sum, ray_trace_count);
+                app_log.info("Mean ray trace time :: {d}ns", .{ray_trace_time_avg});
             }
 
             try wl_surface.attach(&proxy, .{
