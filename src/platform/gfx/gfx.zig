6c4906d6ed5d07fb4b13387a0acbd407bc65216f
Liam Malone <maloneliam@proton.me>
2026-03-02 02:17:44 +0000

    fix gfx formats

--- src/platform/gfx/gfx.zig
+++ src/platform/gfx/gfx.zig
@@ -29,58 +29,53 @@ pub const Float4 = packed union {
   rgba: packed struct (u128) { r: f32, g: f32, b: f32, a: f32 },
   vec: @Vector(4, f32),
 };
-
 pub const Format = enum(u32) {
   invalid = 0,
-  xrgb32,   // no alpha, R in high bits memory order: [B,G,R,X]
-  xbgr32,   // no alpha, B in high bits memory order: [R,G,B,X]
-  rgbx32,   // no alpha, memory order: [X,B,G,R]
-  argb32,   // with alpha, memory order: [B,G,R,A]
-  abgr32,   // with alpha, memory order: [R,G,B,A]
-  rgba32,   // with alpha, memory order: [A,B,G,R]
+  rgba32,   // vk: r8g8b8a8_unorm,         drm: abgr8888  [A,B,G,R] in memory
+  bgra32,   // vk: b8g8r8a8_unorm,         drm: argb8888  [B,G,R,A] in memory
+  abgr32,   // vk: a8b8g8r8_unorm_pack32,  drm: rgba8888  [R,G,B,A] in memory
+  rgbx32,   // vk: r8g8b8a8_unorm,         drm: xbgr8888  [X,B,G,R] in memory
+  bgrx32,   // vk: b8g8r8a8_unorm,         drm: xrgb8888  [B,G,R,X] in memory
 
   pub fn fromDrm(fmt: Drm.Format) ?Format {
     return switch (fmt) {
-      .xrgb8888 => .xrgb32,
-      .xbgr8888 => .xbgr32,
-      .rgbx8888 => .rgbx32,
-      .argb8888 => .argb32,
-      .abgr8888 => .abgr32,
-      .rgba8888 => .rgba32,
+      .abgr8888 => .rgba32,
+      .argb8888 => .bgra32,
+      .rgba8888 => .abgr32,
+      .xbgr8888 => .rgbx32,
+      .xrgb8888 => .bgrx32,
       else      => null,
     };
   }
 
   pub fn fromVk(fmt: vk.Format) ?Format {
     return switch (fmt) {
-      .b8g8r8a8_unorm  => .argb32,
-      .r8g8b8a8_unorm  => .abgr32,
-      .a8b8g8r8_unorm_pack32 => .rgba32,
-      else             => null,
+      .r8g8b8a8_unorm        => .rgba32,
+      .b8g8r8a8_unorm        => .bgra32,
+      .a8b8g8r8_unorm_pack32 => .abgr32,
+      else                   => null,
     };
   }
 
   pub fn toDrm(fmt: Format) Drm.Format {
     return switch (fmt) {
       .invalid => .invalid,
-      .xrgb32  => .xrgb8888,
-      .xbgr32  => .xbgr8888,
-      .rgbx32  => .rgbx8888,
-      .argb32  => .argb8888,
-      .abgr32  => .abgr8888,
-      .rgba32  => .rgba8888,
+      .rgba32  => .abgr8888,
+      .bgra32  => .argb8888,
+      .abgr32  => .rgba8888,
+      .rgbx32  => .xbgr8888,
+      .bgrx32  => .xrgb8888,
     };
   }
 
   pub fn toVk(fmt: Format) vk.Format {
     return switch (fmt) {
       .invalid => .undefined,
-      .xrgb32  => .b8g8r8a8_unorm,  // X treated as ignored alpha
-      .xbgr32  => .r8g8b8a8_unorm,
-      .rgbx32  => .a8b8g8r8_unorm_pack32,
-      .argb32  => .b8g8r8a8_unorm,
-      .abgr32  => .r8g8b8a8_unorm,
-      .rgba32  => .a8b8g8r8_unorm_pack32,
+      .rgba32  => .r8g8b8a8_unorm,
+      .bgra32  => .b8g8r8a8_unorm,
+      .abgr32  => .a8b8g8r8_unorm_pack32,
+      .rgbx32  => .r8g8b8a8_unorm,  // x = ignored alpha
+      .bgrx32  => .b8g8r8a8_unorm,
     };
   }
 };
