3318e90f70d9fef494aacac760fdfb06d13bc7b8
Liam Malone <maloneliam@proton.me>
2026-04-14 18:08:47 +0100

    fix typo... though type is likely fully unused...

--- src/platform/gfx/gfx.zig
+++ src/platform/gfx/gfx.zig
@@ -7,7 +7,7 @@ pub const ColorRgba = packed union {
   uint: u32,
 
   pub fn rgba32_(r: u8, g: u8, b: u8, a: u8) ColorRgba {
-    return .{ .rba32 = .{ .r = r, .g = g, .b = b, .a = a } };
+    return .{ .rgba32 = .{ .r = r, .g = g, .b = b, .a = a } };
   }
 
   pub const Rgba32 = packed struct (u32) {
